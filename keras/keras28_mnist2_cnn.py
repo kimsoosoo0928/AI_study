dd24e4b2ffc3f4653ef5868f8d0b09601ae61bea
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-07-18 23:31:07 +0900

    07-18

--- keras/keras28_mnist2_cnn.py
+++ keras/keras28_mnist2_cnn.py
@@ -1,21 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.datasets import cifar100
+from tensorflow.keras.datasets import mnist
 
 
 # 1. data
-(x_train, y_train), (x_test, y_test) = cifar100.load_data() # (50000, 32, 32, 3) (50000, 1), (10000, 32, 32, 3) (10000, 1)
+(x_train, y_train), (x_test, y_test) = mnist.load_data() # (60000, 28, 28) (60000,) (10000, 28, 28) (10000,)
 
-x_train = x_train.reshape(50000, 32, 32, 3)/255. # (50000, 32, 32, 3)
-x_test = x_test.reshape(10000, 32, 32, 3)/255. # (10000, 28, 32, 3)
+x_train = x_train.reshape(60000, 28, 28, 1)/255. # (60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)/255. # (10000, 28, 28, 1)
 
 from sklearn.preprocessing import OneHotEncoder
 one = OneHotEncoder()
 y_train = y_train.reshape(-1,1)
 y_test = y_test.reshape(-1,1)
 one.fit(y_train)
-y_train = one.transform(y_train).toarray() 
-y_test = one.transform(y_test).toarray() 
+y_train = one.transform(y_train).toarray() # (60000, 10)
+y_test = one.transform(y_test).toarray() # (10000, 10)
 
 # 2. model
 from tensorflow.keras.models import Sequential
@@ -24,7 +24,7 @@ from tensorflow.python.keras.layers.core import Flatten
 
 model = Sequential()
 model.add(Conv2D(filters=32, kernel_size=(5, 5),                          
-                        padding='same', activation='relu' ,input_shape=(32, 32, 3))) 
+                        padding='same', activation='relu' ,input_shape=(28, 28, 1))) 
 model.add(Conv2D(32, (5, 5), padding='same', activation='relu'))                   
 model.add(MaxPool2D())                                         
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))                   
